b57855e17314943e22287e11523e0d5bcaaecce9
masumeh <khanimasume88@gmail.com>
2023-11-07 15:44:47 +0330

    add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //Primary
+        "Orange": "hsl(25, 97%, 53%)",
+        // Neutral
+        "White": "hsl(0, 0%, 100%)",
+        "LightGrey": "hsl(217, 12%, 63%)",
+        "MediumGrey": "hsl(216, 12%, 54%)",
+        "DarkBlue": "hsl(213, 19%, 18%)",
+        "VeryDarkBlue": "hsl(216, 12%, 8%)",
+      },
+      fontFamily: {
+        'Overpass': ['Overpass', 'sans'],
+      },
+    },
   },
   plugins: [],
-}
+};
