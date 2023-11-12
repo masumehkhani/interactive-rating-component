012064f78a782521bcfa0db5a2dab3fe7ae03b0b
masumeh <khanimasume88@gmail.com>
2023-11-12 16:50:47 +0330

    project is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import Container from "./components/Container"
+import InteractiveRaiting from "./components/InteractiveRaiting"
+import ThankYou from "./components/ThankYou"
+import {useState} from 'react'
 
 const App = () => {
+  const [selectedRating, setSelectedRating] = useState<number|null>(null);
+  const [showThankYou, setShowThankYou] = useState(false);
+ const selectedRatingHandler =(item:number)=>{
+  setSelectedRating(item)
+  setShowThankYou(true)
+ }
   return (
-    <div className="font-Overpass">
+    <Container className="font-Overpass w-full h-screen flex justify-center items-center bg-black">
+      {showThankYou?<ThankYou selectedRating={selectedRating}/>:<InteractiveRaiting onclickItem={selectedRatingHandler}/>}
       
-    </div>
+      
+    </Container>
   )
 }
 
